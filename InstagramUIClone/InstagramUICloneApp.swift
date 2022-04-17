f612589ac081a4017057fb688d698a25acc2b2b0
Naman1897 <namansingh1897@gmail.com>
2022-04-18 00:58:19 +0530

    Initial Commit

---
+++ InstagramUIClone/InstagramUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramUICloneApp.swift
+//  InstagramUIClone
+//
+//  Created by Naman Singh on 18/04/22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
